11afacfe25c81a2743d831e0e82197290a3d5303
Jusearco <araujoelcrack@gmail.com>
2025-02-27 11:37:20 -0500

    agrego fact

---
+++ fact.py
@@ -0,0 +1,15 @@
+def factorial (n):
+    fac_numero = 1
+    for i in range(1,n+1):
+        fac_numero = fac_numero*i
+    return(fac_numero)
+
+
+
+numero = int(input("inserte un numero: "))
+
+w = factorial(numero) 
+
+
+
+print(w)
